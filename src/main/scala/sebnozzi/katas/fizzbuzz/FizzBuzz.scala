34fda7a9a748b27715b8f70862b24cf3cce3d997
sebnozzi <sen@192.168.0.12>
2013-04-01 01:37:22 +0200

    Word separator. Readability.

--- src/main/scala/sebnozzi/katas/fizzbuzz/FizzBuzz.scala
+++ src/main/scala/sebnozzi/katas/fizzbuzz/FizzBuzz.scala
@@ -9,19 +9,19 @@ object FizzBuzz {
   def fizzBuzzOf(number: Int): String = {
     specialWordsFor(number: Int) match {
       case Seq() => number.toString
-      case allWords => allWords.mkString(" ")
+      case matchingWords => matchingWords.mkString("-")
     }
   }
 
-  private case class SpecialWord(word: String, when: (Int) => Boolean) {
-    def correspondsTo(number: Int): Boolean = when(number)
+  private case class SpecialWord(word: String, condition: (Int) => Boolean) {
+    def correspondsTo(number: Int): Boolean = condition(number)
     override def toString = word
   }
 
   private lazy val specialWords = Seq(
-    new SpecialWord("Fizz", when = { _.isDivisibleByOrHasDigit(3) }),
-    new SpecialWord("Buzz", when = { _.isDivisibleByOrHasDigit(5) }),
-    new SpecialWord("Wizz", when = { _.isDivisibleByOrHasDigit(7) }))
+    new SpecialWord("Fizz", condition = { number => number.isDivisibleByOrHasDigit(3) }),
+    new SpecialWord("Buzz", condition = { number => number.isDivisibleByOrHasDigit(5) }),
+    new SpecialWord("Wizz", condition = { number => number.isDivisibleByOrHasDigit(7) }))
 
   private def specialWordsFor(number: Int): Seq[SpecialWord] = {
     specialWords.filter { word => word.correspondsTo(number) }
--- src/test/scala/sebnozzi/katas/fizzbuzz/FizzBuzzTest.scala
+++ src/test/scala/sebnozzi/katas/fizzbuzz/FizzBuzzTest.scala
@@ -4,37 +4,37 @@ import org.scalatest.FunSuite
 import FizzBuzz._
 
 class FizzBuzzSuite extends FunSuite {
-  
+
   test("1 translates to 1") {
-    assert("1" === fizzBuzzOf(1))
-  }
-  
-  test("3 translates to Fizz") {
-    assert("Fizz" === fizzBuzzOf(3))
-  }
-  
-  test("5 translates to Buzz") {
-    assert("Buzz" === fizzBuzzOf(5))
-  }
-  
-  test("15 translates too Fizz Buzz") {
-    assert("Fizz Buzz" === fizzBuzzOf(15))
-  }
-  
-  test("6 translates to Fizz") {
-    assert("Fizz" === fizzBuzzOf(6))
-  }
-  
-  test("10 translates to Buzz") {
-    assert("Buzz" === fizzBuzzOf(10))
+    assert(fizzBuzzOf(1) === "1")
   }
 
-  test("30 translates to Fizz Buzz") {
-    assert("Fizz Buzz" === fizzBuzzOf(30))
+  test("3 translates to Fizz") {
+    assert(fizzBuzzOf(3) === "Fizz")
+  }
+
+  test("5 translates to Buzz") {
+    assert(fizzBuzzOf(5) === "Buzz")
+  }
+
+  test("15 translates too Fizz-Buzz") {
+    assert(fizzBuzzOf(15) === "Fizz-Buzz")
+  }
+
+  test("6 translates to Fizz") {
+    assert(fizzBuzzOf(6) === "Fizz")
+  }
+
+  test("10 translates to Buzz") {
+    assert(fizzBuzzOf(10) === "Buzz")
+  }
+
+  test("30 translates to Fizz-Buzz") {
+    assert(fizzBuzzOf(30) === "Fizz-Buzz")
   }
 
   test("2 translates to 2") {
-    assert("2" === fizzBuzzOf(2))
+    assert(fizzBuzzOf(2) === "2")
   }
 
   test("game to upper bound 1") {
@@ -54,32 +54,32 @@ class FizzBuzzSuite extends FunSuite {
   }
 
   test("game to upper bound 15") {
-    assert(fizzBuzzUpTo(15).last === "Fizz Buzz")
-    assert(fizzBuzzUpTo(15)(14) === "Fizz Buzz")
+    assert(fizzBuzzUpTo(15).last === "Fizz-Buzz")
+    assert(fizzBuzzUpTo(15)(14) === "Fizz-Buzz")
   }
-  
+
   test("7 translates to Wizz") {
-    assert("Wizz" === fizzBuzzOf(7))
+    assert(fizzBuzzOf(7) === "Wizz")
   }
 
   test("21 translates to Fizz Wizz") {
-    assert(fizzBuzzOf(21) === "Fizz Wizz")
+    assert(fizzBuzzOf(21) === "Fizz-Wizz")
   }
 
-  test("105 translates to Fizz Buzz Wizz") {
-    assert(fizzBuzzOf(105) === "Fizz Buzz Wizz")
+  test("105 translates to Fizz-Buzz-Wizz") {
+    assert(fizzBuzzOf(105) === "Fizz-Buzz-Wizz")
   }
 
-  test("35 translates to Fizz Buzz Wizz") {
-    assert(fizzBuzzOf(35) === "Fizz Buzz Wizz")
+  test("35 translates to Fizz-Buzz-Wizz") {
+    assert(fizzBuzzOf(35) === "Fizz-Buzz-Wizz")
   }
 
   test("13 translates to Fizz") {
     assert(fizzBuzzOf(13) === "Fizz")
   }
 
-  test("53 translates to Fizz Buzz") {
-    assert(fizzBuzzOf(53) === "Fizz Buzz")
+  test("53 translates to Fizz-Buzz") {
+    assert(fizzBuzzOf(53) === "Fizz-Buzz")
   }
 
   test("17 translates to Wizz") {
